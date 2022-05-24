1d39749dc43aedf4d80853ad8d18a67c8ded0575
yi quan <muleforprojects@gmail.com>
2022-05-24 12:03:54 +0800

    Update web topic 3 coursera

--- Web Development/coursera/topic3/bioweb/genedata/views.py
+++ Web Development/coursera/topic3/bioweb/genedata/views.py
@@ -1,36 +1,93 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .models import *
+from .forms import *
 
 
-def index(request):
+class GeneList(ListView):
+    model = Gene
+    context_object_name = 'master_genes'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if 'poslist' in self.request.get_full_path():
+            context['genes'] = Gene.objects.filter(
+                entity__exact='Chromosome').filter(sense__startswith='+')
+
+        if 'type' in self.kwargs:
+            if 'Chromosome' in self.kwargs['type'] or 'Plasmid' in self.kwargs['type']:
+                context['genes'] = Gene.objects.filter(
+                    entity__exact=self.kwargs['type'])
+        return context
+
+    def get_template_names(self):
+        if 'poslist' in self.request.get_full_path():
+            return('genedata/list.html')
+        if 'type' in self.kwargs:
+            if 'Chromosome' in self.kwargs['type'] or 'Plasmid' in self.kwargs['type']:
+                return('genedata/list.html')
+        return 'genedata/index.html'
+
+
+class GeneDetail(DetailView):
+    model = Gene
+    context_object_name = 'gene'
+    template_name = 'genedata/gene.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['master_genes'] = Gene.objects.all()
+        return context
+
+
+class GeneDelete(DeleteView):
+    model = Gene
+    success_url = "/"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['master_genes'] = Gene.objects.all()
+        return context
+
+
+class GeneUpdate(UpdateView):
+    model = Gene
+    fields = ['gene_id', 'entity', 'start', 'stop',
+              'sense', 'start_codon', 'sequencing', 'ec']
+    template_name_suffix = '_update_form'
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['master_genes'] = Gene.objects.all()
+        return context
+
+
+def create_ec(request):
     master_genes = Gene.objects.all()
-    return render(request, 'genedata/index.html', {'master_genes': master_genes})
+
+    if request.method == 'POST':
+        form = ECForm(request.POST)
+        if form.is_valid():
+            ec = EC()
+            ec.ec_name = form.cleaned_data['ec_name']
+            ec.save()
+            return HttpResponseRedirect('/create_ec/')
+
+    else:
+        ecs = EC.objects.all()
+        form = ECForm()
+        return render(request, 'genedata/ec.html', {'form': form, 'ecs': ecs, 'master_genes': master_genes})
 
 
-def gene(request, pk):
-    gene = Gene.objects.get(pk=pk)
-    gene.access += 1
-    print("Gene record:", pk, "access count:", str(gene.access))
-    gene.save()
-    master_genes = Gene.objects.all()
-    return render(request, 'genedata/gene.html', {'gene': gene, 'master_genes': master_genes})
+class GeneCreate(CreateView):
+    model = Gene
+    template_name = 'genedata/create_gene.html'
+    form_class = GeneForm
+    success_url = '/create_gene/'
 
-
-def list(request, type):
-    genes = Gene.objects.filter(entity__exact=type)
-    master_genes = Gene.objects.all()
-    return render(request, 'genedata/list.html', {'genes': genes, 'type': type, 'master_genes': master_genes})
-
-
-def poslist(request):
-    genes = Gene.objects.filter(
-        entity__exact='Chromosome').filter(sense__startswith='+')
-    master_genes = Gene.objects.all()
-    return render(request, 'genedata/list.html', {'genes': genes, 'type': 'PosList', 'master_genes': master_genes})
-
-
-def delete(request, pk):
-    GeneAttributeLink.objects.filter(gene_id=pk).delete()
-    Gene.objects.filter(pk=pk).delete()
-    return HttpResponseRedirect("/")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['master_genes'] = Gene.objects.all()
+        return context
