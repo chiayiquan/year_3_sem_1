7020ae9ce3b512ec513696d953f335a70e0afc11
yiquan <muleforprojects@gmail.com>
2022-07-11 19:29:45 +0800

    Fix web dev

--- Web Development/coursera/topic6/image_store/store/tasks.py
+++ Web Development/coursera/topic6/image_store/store/tasks.py
@@ -4,18 +4,17 @@ from PIL import Image as img
 import io
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-
 @shared_task
 def make_thumbnail(record_pk):
-    record = Image.objects.get(pk=record_pk)
+ record = Image.objects.get(pk=record_pk)
+ image = img.open('images/'+str(record.image))
 
-    image = img.open('/images/'+str(record.image))
-    x_scale_factor = image.size[0]/100
-    thumbnail = image.resize((100, int(image.size[1]/x_scale_factor)))
-    thumbnail.save('test.jpg')
+ x_scale_factor = image.size[0]/100
+ thumbnail = image.resize((100, int(image.size[1]/x_scale_factor)))
+ thumbnail.save("test.jpg")
 
-    byteArr = io.BytesIO()
-    thumbnail.save(byteArr, format='jpeg')
-    file = SimpleUploadedFile('thumb_'+str(record.image), byteArr.getvalue())
-    record.thumbnail = file
-    record.save()
+ byteArr = io.BytesIO()
+ thumbnail.save(byteArr, format='jpeg')
+ file = SimpleUploadedFile("thumb_"+str(record.image), byteArr.getvalue())
+ record.thumbnail = file
+ record.save()
