6a9a1b38a0aedf5f1c934eaebe47c40c70f99cbc
yi quan <muleforprojects@gmail.com>
2022-12-01 18:54:37 +0800

    Add 3D topic 4

---
+++ 3D graphics/Formula/vector_position.js
@@ -0,0 +1,51 @@
+const time = 1; // change accordingly if is provided
+const timeStep = 0.1; // change accordingly if is provided
+const force = [300, 0, 0]; // change accordingly if is provided
+const mass = 50; // change accordingly if is provided
+
+let initialVector = [0, 0, 0]; // change accordingly if is provided
+let initialPosition = [0, 0, 0]; // change accordingly if is provided
+const givenAcceleration = [0, 0, 0]; // change this if acceleration is provided
+
+const calculatedAcceleration = force.map((f) =>
+  isFinite(f / mass) ? f / mass : 0
+);
+
+const acceleration = calculatedAcceleration.every((acc) => acc === 0)
+  ? givenAcceleration
+  : calculatedAcceleration;
+
+[...new Array(Math.floor(time / timeStep)).keys()].forEach((index) => {
+  // index start from 0
+  const step = ((index + 1) * timeStep).toFixed(2);
+
+  console.log(
+    `The position at ${step} is [${initialPosition
+      .map(
+        (position, index) => `${position}+${initialVector[index]}*${timeStep}`
+      )
+      .join(", ")}] = [${calculatePosition()}]`
+  );
+
+  console.log(
+    `The vector at ${step} is [${initialVector
+      .map((vector, index) => `${vector}+${acceleration[index]}*${timeStep}`)
+      .join(", ")}] = [${calculateVector()}]`
+  );
+
+  console.log("==========================================================");
+});
+
+function calculatePosition() {
+  return (initialPosition = initialPosition.map((position, index) => {
+    const calculatedPosition = position + initialVector[index] * timeStep;
+    return Math.round((calculatedPosition + Number.EPSILON) * 10000) / 10000;
+  }));
+}
+
+function calculateVector() {
+  return (initialVector = initialVector.map((vector, index) => {
+    const calculatedVector = vector + acceleration[index] * timeStep;
+    return Math.round((calculatedVector + Number.EPSILON) * 10000) / 10000;
+  }));
+}
